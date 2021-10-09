085a20dbc2c9ea53277fa34067d3ad6ea1f415cb
Vasya <vpetruin@inbox.ru>
2021-10-09 14:54:40 +0300

    implemented adding news when scrolling

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,31 +1,36 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { newsList } from "../../utils/constance";
 import { Novetly } from "../Novetly/Novetly";
 import "./app.css";
 
 function App() {
-  const [renderNewsList, setRenderNewsList] = useState([]);
+  const [countNews, setCountNews] = useState(10);
+  const wrapper = useRef(null);
 
-  const generateInitialList = (list) => {
-    let initialList = [];
-
-    for (let i = 0; i < 10; i++) {
-      initialList.push(list[i]);
-      setRenderNewsList(initialList);
+  const scrollHandler = (e) => {
+    const { target } = e;
+    if (wrapper) {
+      if (target.scrollWidth - target.scrollLeft - 260 <= target.offsetWidth) {
+        setCountNews((prev) => prev + 1);
+      }
     }
   };
 
   useEffect(() => {
-    generateInitialList(newsList);
-  }, []);
-
-  console.log(renderNewsList);
+    const { current } = wrapper;
+    current.addEventListener("scroll", scrollHandler);
+    return () => {
+      if (countNews.length === newsList.length) {
+        current.removeEventListener("scroll", scrollHandler);
+      }
+    };
+  });
 
   return (
     <div className="app">
       <h1 className="center-align">News</h1>
-      <div className="app__wrapper">
-        {renderNewsList.map((item) => (
+      <div className="app__wrapper" ref={wrapper}>
+        {newsList.slice(0, countNews).map((item) => (
           <Novetly novetly={item} key={item.id} />
         ))}
       </div>
